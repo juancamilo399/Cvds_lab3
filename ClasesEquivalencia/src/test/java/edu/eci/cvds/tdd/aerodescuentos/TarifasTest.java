1017487e12246b73dae35647931c5d72c6099601
juan angel <juan.angel-h@mail.escuelaing.edu.co>
2019-08-24 21:06:30 -0500

    pruebas

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -1,7 +1,7 @@
 package edu.eci.cvds.tdd.aerodescuentos;
 import org.junit.Assert;
 import org.junit.Test;
-import edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos;
+//import edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos;
 
 public class TarifasTest {
 
@@ -11,21 +11,77 @@ public class TarifasTest {
 		double actual = CalculadorDescuentos.calculoTarifa(	1000, 21, 30);
 		Assert.assertEquals(850.0, actual,0.0000000001);
 	}
+	
+	@Test 
+	public void tarifasTestDescuentoAntelacion2() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 22, 30);
+		Assert.assertEquals(850.0, actual,0.0000000001);
+	}
+	
+	@Test 
+	public void tarifasTestDescuentoSinAntelacion() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 20, 30);
+		Assert.assertNotEquals(850.0, actual,0.0000000001);
+	}
+	
 	@Test 
 	public void tarifasTestDescuentoMenorDeEdad() {
-		double actual = CalculadorDescuentos.calculoTarifa(	1000, 15, 5);
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 17, 16);
 		Assert.assertEquals(950.0, actual,0.0000000001);
 	}
+	
+	
+	@Test 
+	public void tarifasTestDescuentoMenorDeEdad2() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 17, 17);
+		Assert.assertEquals(950.0, actual,0.0000000001);
+	}
+	
+	@Test 
+	public void tarifasTestSinDescuentoMenorDeEdad() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 21, 18);
+		Assert.assertNotEquals(950.0, actual,0.0000000001);
+	}
+	
 	@Test 
 	public void tarifasTestDescuentoTerceraEdad() {
-		double actual = CalculadorDescuentos.calculoTarifa(1000, 15, 66);
+		double actual = CalculadorDescuentos.calculoTarifa(1000, 17, 66);
 		Assert.assertEquals(920.0, actual,0.0000000001);
 	}
+	
+	
+	
+	@Test 
+	public void tarifasTestDescuentoTerceraEdad2() {
+		double actual = CalculadorDescuentos.calculoTarifa(1000, 17, 68);
+		Assert.assertEquals(920.0, actual,0.0000000001);
+	}
+	
+	@Test 
+	public void tarifasTestSinDescuentoTerceraEdad() {
+		double actual = CalculadorDescuentos.calculoTarifa(1000, 21, 65);
+		Assert.assertNotEquals(920.0, actual,0.0000000001);
+	}
+	
 	@Test 
 	public void tarifasTestDescuentoAntelacionYMenorDeEdad() {
-		double actual = CalculadorDescuentos.calculoTarifa(	1000, 21, 5);
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 21, 17);
 		Assert.assertEquals(800.0, actual,0.0000000001);
 	}
+	
+	@Test 
+	public void tarifasTestDescuentoAntelacionYMenorDeEdad2() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 22, 16);
+		Assert.assertEquals(800.0, actual,0.0000000001);
+	}
+	
+	@Test 
+	public void tarifasTestSinDescuentoAntelacionYMenorDeEdad() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 20, 18);
+		Assert.assertNotEquals(800.0, actual,0.0000000001);
+	}
+	
+	
 	@Test 
 	public void tarifasTestDescuentoAntelacionYTerceraEdad() {
 		double actual = CalculadorDescuentos.calculoTarifa(	1000, 21, 66);
@@ -33,15 +89,35 @@ public class TarifasTest {
 	}
 	
 	@Test 
-	public void tarifasTestSinDescuentos() {
-		double actual = CalculadorDescuentos.calculoTarifa(	1000,5, 30);
+	public void tarifasTestDescuentoAntelacionYTerceraEdad2() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 22, 67);
+		Assert.assertEquals(770.0, actual,0.0000000001);
+	}
+	
+	@Test 
+	public void tarifasTestSinDescuentoAntelacionYTerceraEdad() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000, 20, 65);
+		Assert.assertNotEquals(770.0, actual,0.0000000001);
+	}
+	
+	
+	@Test 
+	public void tarifasTestSinDescuentos2() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000,20,18);
 		Assert.assertEquals(1000.0, actual,0.0000000001);
 	}
-	/**
+	
 	@Test 
-	public void tarifasTestDescuentoAntelacion() {
-		double actual = CalculadorDescuentos.calculoTarifa(	1000, 21, 30);
-		Assert.assertEquals(850.0, actual,0.0000000001);
+	public void tarifasTestSinDescuentos() {
+		double actual = CalculadorDescuentos.calculoTarifa(	1000,19,19);
+		Assert.assertEquals(1000.0, actual,0.0000000001);
+	}
+	
+	
+	/**
+	@Test(expected = ExcepcionParametrosInvalidos.class)
+	public void tarifasTestParametrosIncorrectos() {
+		double actual = CalculadorDescuentos.calculoTarifa(	-1, 0, 1);
 	}
 	*/
 
--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -9,15 +9,17 @@ public class RegistryTest {
 
     @Test
     public void validateRegistryResultIsDead() {
-
         Person person = new Person("pepitoelmuertito",99,99,Gender.MALE,false);
-
         RegisterResult result = registry.registerVoter(person);
-
         Assert.assertEquals(RegisterResult.DEAD, result);
     }
     
-   
+    @Test
+    public void validateRegistryResultNotIsDead() {
+        Person person = new Person("pepitoelmuertito",99,99,Gender.MALE,true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertNotEquals(RegisterResult.DEAD, result);
+    }
     
     @Test
     public void validateRegistryResultInvalidAge() {
@@ -25,21 +27,63 @@ public class RegistryTest {
     	RegisterResult result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.INVALID_AGE, result);
     }
+  
+    @Test
+    public void validateRegistryResultNotInvalidAge2() {
+    	Person person= new Person("pepitonegativo",98,0,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(person);
+        Assert.assertNotEquals(RegisterResult.INVALID_AGE, result);
+    }   
     
-   
+    @Test
+    public void validateRegistryResultNotInvalidAge3() {
+    	Person person= new Person("pepitonegativo",98,1,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(person);
+        Assert.assertNotEquals(RegisterResult.INVALID_AGE, result);
+    } 
     
     @Test
     public void validateRegistryResultInvalidAge2() {
-    	Person person= new Person("matusalen",97,300,Gender.MALE,true);
+    	Person person= new Person("matusalen",97,125,Gender.MALE,true);
     	RegisterResult result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.INVALID_AGE, result);
     }
+    
+    @Test
+    public void validateRegistryResultNotInvalidAge4() {
+    	Person person= new Person("matusalen",97,124,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(person);
+        Assert.assertNotEquals(RegisterResult.INVALID_AGE, result);
+    }
+    
+    @Test
+    public void validateRegistryResultNotInvalidAge5() {
+    	Person person= new Person("matusalen",97,123,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(person);
+        Assert.assertNotEquals(RegisterResult.INVALID_AGE, result);
+    }
+    
     @Test
     public void validateRegistryResultUnderAge() {
     	Person person= new Person("luisito",96,17,Gender.MALE,true);
     	RegisterResult result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.UNDERAGE, result);
     }
+    
+    @Test
+    public void validateRegistryResultNotUnderAge() {
+    	Person person= new Person("luisito",96,18,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(person);
+        Assert.assertNotEquals(RegisterResult.UNDERAGE, result);
+    }
+    
+    @Test
+    public void validateRegistryResultNotUnderAge2() {
+    	Person person= new Person("luisito",96,19,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(person);
+        Assert.assertNotEquals(RegisterResult.UNDERAGE, result);
+    }
+    
     @Test
     public void validateRegistryResultDuplicate() {
     	Person person= new Person("juano",95,19,Gender.MALE,true);
@@ -50,8 +94,24 @@ public class RegistryTest {
     }
     
     @Test
+    public void validateRegistryResultNotDuplicate() {
+    	Person person= new Person("juano",95,19,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.VALID, result);
+        Assert.assertNotEquals(RegisterResult.DUPLICATED, result);
+    }
+    
+    
+    @Test
     public void validateRegistryResultValid() {
     	Person person= new Person("luisa",93,18,Gender.FEMALE,true);
+    	RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.VALID, result);
+    }
+    
+    @Test
+    public void validateRegistryResultValid2() {
+    	Person person= new Person("luisa",93,19,Gender.FEMALE,true);
     	RegisterResult result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.VALID, result);
     }
